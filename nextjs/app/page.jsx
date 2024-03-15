cd6e040cac663b238eacf47f4388b00406570bb3
Seba Mediza <sebamediza@hotmail.com>
2024-03-15 19:03:40 -0300

    algo de la primer interfaz

---
+++ nextjs/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <section>
+            <div className='bg-neutral-800'>Hola</div>
+            <div></div>
+        </section>
+    )
+}
+
+export default page
---
+++ nextjs/components/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button({legend}){
+    return (
+        <button>{legend}</button>
+    );
+}
