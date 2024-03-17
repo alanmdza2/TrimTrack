6c7bb865b4cfb7fd1bec16918c20ab1fcadfa596
Leandro Richter <leandrorichter2000@gmail.com>
2024-03-17 16:43:08 -0300

    modified:   spring/src/main/java/com/is/spring/turnos/model/Servicio.java 	modified:   spring/src/main/java/com/is/spring/turnos/model/Turno.java 	modified:   spring/src/main/java/com/is/spring/turnos/model/Usuario.java

--- spring/src/main/java/com/is/spring/turnos/model/Servicio.java
+++ spring/src/main/java/com/is/spring/turnos/model/Servicio.java
@@ -1,6 +1,8 @@
 package com.is.spring.turnos.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,6 +12,7 @@ import jakarta.persistence.Table;
 public class Servicio {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private int idservicio;
     @Column
--- spring/src/main/java/com/is/spring/turnos/model/Usuario.java
+++ spring/src/main/java/com/is/spring/turnos/model/Usuario.java
@@ -2,6 +2,8 @@ package com.is.spring.turnos.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,6 +12,7 @@ import jakarta.persistence.Table;
 public class Usuario {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private int idusuario;
     @Column
