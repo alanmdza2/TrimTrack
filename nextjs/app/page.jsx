c52799b32d5e6b90a4943366aa498184b00f9366
Seba Mediza <sebamediza@hotmail.com>
2024-03-23 12:56:45 -0300

    conexion api

--- nextjs/app/page.jsx
+++ nextjs/app/page.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+'use-client'
+import React, { useEffect } from 'react'
 import Servicio from '@/components/Servicio'
 import Button from '@/components/Button'
 
 const page = () => {
+    const getData = async (e) => {
+        const api = 'http://localhost:8080/api/turno';
+        const data = await fetch(api, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                //'bearer': 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjMiLCJpYXQiOjE3MTEyMDU0MDIsImV4cCI6MTcxMTIwNjg0Mn0.pHifingm8l4e5eQJjKQ68bW8GppyDo3oxzad8QzdfEc'
+            }
+        });
+        const res = await data.json();
+        console.log(res);
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+
     return (
         <section className='bg-'>
             <div>
