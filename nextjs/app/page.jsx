7e0c45881ea80bdd9df231251e52cddda1f0135b
Seba Mediza <sebamediza@hotmail.com>
2024-03-24 21:32:51 -0300

    front casi listo

--- nextjs/app/page.jsx
+++ nextjs/app/page.jsx
@@ -1,10 +1,8 @@
 'use client'
-
 import Servicio from '@/components/Servicio'
-import Button from '@/components/Button'
 import Calendar from '@/components/Calendar'
 import Appointment from '@/components/Appointment'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const page = () => {
     const [opcion, setOpcion] = useState("");
@@ -12,23 +10,27 @@ const page = () => {
         setOpcion(e.target.value)
         console.log(opcion);
     }
-    /*const getData = async (e) => {
-        const api = 'http://localhost:8080/api/turno';
-        const data = await fetch(api, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                //'bearer': 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjMiLCJpYXQiOjE3MTEyMDU0MDIsImV4cCI6MTcxMTIwNjg0Mn0.pHifingm8l4e5eQJjKQ68bW8GppyDo3oxzad8QzdfEc'
-            }
-        });
-        const res = await data.json();
-        console.log(res);
+    const getData = async () => {
+        try {
+            const api = 'https://trimtrack-production.up.railway.app/api/turno';
+            const data = await fetch(api, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3OCIsImlhdCI6MTcxMTMyMjg4MCwiZXhwIjoxNzExMzI0MzIwfQ.0Fo_T1Mm26U5EFcOBYRaqXXcIiHcufYewm2VUWHBVWs'
+                }
+            });
+            const res = await data.json();
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     useEffect(() => {
         getData();
     }, [])
-    */
+
     return (
         <div className='h-screen w-full bg-[#303030] flex justify-around'>
             <div className="w-2/3 mt-5 content-center">
@@ -49,37 +51,35 @@ const page = () => {
                             </div>
                         </div>
                         <div className='grid grid-cols-2 gap-8 items-center'>
-                            <div className="text-l w-full font-1 text-white pt-7 colspan-1 ">
-                                <u>Tienes alguna duda? Conoce nuestras politicas de reserva</u>
-                            </div>
                             {/* <button className="w-2/3 h-2/3 justify-self-end colspan-1 bg-[#5865F2] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">
                                 Siguiente
                             </button> */}
                         </div>
                         {opcion && (
-                            <div className="h-full max-w-full bg-[#303030] flex justify-around">
-                                <div className=" pt-8 grid grid-cols-2 content-center gap-x-14 ">
-                                    <div className="colspan-1">
-                                        <div className="text-2xl font-1 text-white pb-10">
-                                            Seleccione una fecha y hora
-                                        </div>
-                                        <div className="background p-7 rounded-xl">
-                                            <div className="calendar-background rounded-xl">
-                                                <Calendar></Calendar>
+                            <div>
+                                <div className="h-full max-w-full bg-[#303030] flex justify-around">
+                                    <div className=" pt-8 grid grid-cols-2 content-center gap-x-14 ">
+                                        <div className="colspan-1">
+                                            <div className="text-2xl font-1 text-white pb-10">
+                                                Seleccione una fecha y hora
                                             </div>
-                                        </div>
-                                        <div className="text-l font-1 text-white pt-7">
-                                            <u>Tienes alguna duda? Conoce nuestras politicas de reserva</u>
-                                        </div>
-                                    </div>
-                                    <div className="colspan-1">
-                                        <div className="pt-16">
-                                            <div className="h-96 p-6 background rounded-xl overflow-y-scroll scroll-smooth">
-                                                <div className="flex flex-col w-full">
-                                                    <Appointment></Appointment>
+                                            <div className="background p-7 rounded-xl">
+                                                <div className="calendar-background rounded-xl">
+                                                    <Calendar />
                                                 </div>
                                             </div>
-                                            {/* <div className="grid grid-cols-2 gap-x-8 pt-8">
+                                            <div className="text-l font-1 text-white pt-7">
+                                                <u>Tienes alguna duda? Conoce nuestras politicas de reserva</u>
+                                            </div>
+                                        </div>
+                                        <div className="colspan-1">
+                                            <div className="pt-16">
+                                                <div className="h-96 p-6 background rounded-xl overflow-y-scroll scroll-smooth">
+                                                    <div className="flex flex-col w-full">
+                                                        <Appointment />
+                                                    </div>
+                                                </div>
+                                                {/* <div className="grid grid-cols-2 gap-x-8 pt-8">
                                                 <button className="colspan-1 bg-[#5865F2] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">
                                                     Atrás
                                                 </button>
@@ -87,6 +87,39 @@ const page = () => {
                                                     Siguiente
                                                 </button>
                                             </div> */}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="h-full max-w-full bg-[#303030] flex justify-around">
+                                        <div>
+                                            Ingrese sus datos:
+                                        </div>
+                                        <div className='fild'>
+                                            <p>Nombre y apellido</p>
+                                            <textarea id='nombre'></textarea>
+                                        </div>
+                                        <div className='fild' style={{ display: "flex" }}>
+                                            <div>
+                                                <p>Nº de documetno</p>
+                                                <textarea id='documento'></textarea>
+                                            </div>
+                                            <div style={{ marginLeft: "auto" }}>
+                                                <p>Telefono</p>
+                                                <textarea id='telefono'></textarea>
+                                            </div>
+                                        </div>
+                                        <div className='fild'>
+                                            <p>Comentarios</p>
+                                            <textarea id="comentario" style={{ height: "120px" }}></textarea>
+                                        </div>
+                                        <div className='fild' style={{ textAlign: "center" }}>
+                                            <p>Estoy de acuerdo con las politicas reservadas</p>
+                                        </div>
+                                        <div className='butons'>
+                                            <button>Finalizar</button>
+                                            <button>Atras</button>
                                         </div>
                                     </div>
                                 </div>
---
+++ nextjs/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="text-l w-full font-1 text-white pt-7 colspan-1 ">
+            <u>Tienes alguna duda? Conoce nuestras politicas de reserva</u>
+        </div>
+    )
+}
+
+export default Footer
